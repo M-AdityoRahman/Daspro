2aa7d512ad1a38cd639d77566c83c987868dc3e8
M-AdityoRahman <144311783+M-AdityoRahman@users.noreply.github.com>
2023-11-02 12:46:45 +0700

    menampilkan orang duduk dibaris ke-3 dgn for loop

--- jobsheet10/Bioskop17.java
+++ jobsheet10/Bioskop17.java
@@ -18,5 +18,9 @@ public class Bioskop17 {
         for (String[] barispenonton : penonton) {
             System.out.println("Panjang baris: " + barispenonton.length);
         }
+        System.out.println("Penonton pada baris ke-3: ");
+        for (int i = 0 ; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
     }
 }

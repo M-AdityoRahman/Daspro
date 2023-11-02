8d8adfb28f1fe06d04c4408bb637a0272d334de8
M-AdityoRahman <144311783+M-AdityoRahman@users.noreply.github.com>
2023-11-02 14:19:44 +0700

    Modifikasi no.12

--- jobsheet10/Bioskop17.java
+++ jobsheet10/Bioskop17.java
@@ -18,9 +18,8 @@ public class Bioskop17 {
         for (String[] barispenonton : penonton) {
             System.out.println("Panjang baris: " + barispenonton.length);
         }
-        System.out.println("Penonton pada baris ke-3: ");
-        for (String i : penonton [2]) {
-            System.out.println(i);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
         }
     }
 }

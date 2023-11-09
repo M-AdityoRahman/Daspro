9b793fa61e386a8bb9cb37134c5ee1b3de3d00b9
M-AdityoRahman <144311783+M-AdityoRahman@users.noreply.github.com>
2023-11-09 16:02:44 +0700

    Number Square

--- jobsheet11/NumberSquare.java
+++ jobsheet11/NumberSquare.java
@@ -1,20 +1,14 @@
 import java.util.Scanner;
-
 public class NumberSquare {
     public static void main(String[] args) {
-        Scanner  sc05 = new Scanner(System.in);
-
+        Scanner  sc = new Scanner(System.in);
         Boolean isNilai = false;
-
         while (!isNilai) {
-
-            System.out.print("Masukkan nilai N (minimal 3): ");
-            int N = sc05.nextInt();
-
+            System.out.print("Masukkan nilai N (min. 3): ");
+            int N = sc.nextInt();
             if (N < 3) {
-                System.out.println("Nilai N kurang dari 3, silakan masukkan nilai kembali!\n");
+                System.out.println("Nilai N minimal 3 ya boss!\n");
             } else {
-
                 for (int i = 1; i <= N; i++) {
                     for (int j = 1; j <= N; j++) {
                         if (i == 1 || i == N || j == 1 || j == N) {

5b8e1a7dfd68f6ce3233bd8c96c77e4a5bb0c2ce
M-AdityoRahman <144311783+M-AdityoRahman@users.noreply.github.com>
2023-11-09 16:28:45 +0700

    Update TugasIndividu1.java

--- jobsheet11/TugasIndividu1.java
+++ jobsheet11/TugasIndividu1.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
-
 public class TugasIndividu1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan nilai N (minimal 3): ");
         int N = input.nextInt();
         input.close();
-
         if (N < 3) {
             System.out.println("Nilai N harus minimal 3.");
         } else {
             for (int i = 1; i <= N; i++) {
+                for (int k = 1; k <= N - i; k++) {
+                    System.out.print(" ");
+                }
                 for (int j = 1; j <= i; j++) {
                     System.out.print(j);
                 }
